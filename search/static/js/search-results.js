af13c2cfc27f7ecfadb6629c532b68914775ea69
Nima Karamooz <nkaramooz@gmail.com>
2017-11-10 12:35:52 -0800

    Fixed show hide functionality for abstract preview

--- search/static/js/search-results.js
+++ search/static/js/search-results.js
@@ -1,5 +1,20 @@
 $(document).ready(function() {
 	$('.expand-btn').on('click', function() {
+
+		var prev = $(this).parent().parent().find(".preview-ellipsis")
+
+		if (prev.length > 0) {
+			prev.removeClass('preview-ellipsis').addClass('preview-full');
+		} else {
+			$(this).parent().parent().find('.preview-full').removeClass('preview-full').addClass('preview-ellipsis');
+		}
+
 		$(this).parent().parent().find(".expanded").toggle();
+
+		if ($(this).text() == 'expand') {
+			$(this).html('<button id="expando">collapse</button>');
+		} else {
+			$(this).html('<button id="expando">expand</button>');
+		}
 	});
 });
