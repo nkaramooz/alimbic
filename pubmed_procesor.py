a25ca305b1f843615376a0538f142c6d90952028
Nima Karamooz <nkaramooz@gmail.com>
2017-12-14 11:53:13 -0800

    final commit before indexing

--- pubmed_procesor.py
+++ pubmed_procesor.py
@@ -31,12 +31,12 @@ def index_doc_from_elem(elem, filter_words_df, filename):
 		or is_issn(elem, '0002-838X') or is_issn(elem, '1532-0650')\
 		or is_issn(elem, '0003-4819') or is_issn(elem, '1539-3704')\
 		or is_issn(elem, '0098-7484') or is_issn(elem, '1538-3598')):
-		print('correct journal')
+
 		json_str = {}
 		json_str = get_journal_info(elem, json_str)
 		if json_str['journal_pub_year'] is not None:
 			if (int(json_str['journal_pub_year']) > 1990):
-				print('correct year')
+	
 				json_str = get_article_info(elem, json_str)
 				
 				if (not bool(set(json_str['article_type']) & set(['Letter', 'Editorial', 'Comment', 'Biography', 'Patient Education Handout', 'News']))):
@@ -49,11 +49,11 @@ def index_doc_from_elem(elem, filter_words_df, filename):
 
 					json_str['title_conceptids'] = get_snomed_annotation(json_str['article_title'], filter_words_df)
 					json_str['abstract_conceptids'] = get_abstract_conceptids(json_str['article_abstract'], filter_words_df)
-					print('after concept')
+	
 					json_str['index_date'] = datetime.datetime.now().strftime("%Y-%m-%d")
 		
 					json_str['index_time'] = datetime.datetime.now().strftime("%H:%M:%S")
-					print('after dt')
+	
 					json_str['filename'] = filename
 					pmid = json_str['pmid']
 					json_str =json.dumps(json_str)
@@ -64,8 +64,8 @@ def index_doc_from_elem(elem, filter_words_df, filename):
 					query_result = es.search(index=INDEX_NAME, body=get_article_query)
 						
 					if query_result['hits']['total'] == 0 or query_result['hits']['total'] > 1:
-						# es.index(index=INDEX_NAME, doc_type='abstract', body=json_obj)
-						print('index')
+						es.index(index=INDEX_NAME, doc_type='abstract', body=json_obj)
+						
 						
 					elif query_result['hits']['total'] == 1:
 						article_id = query_result['hits']['hits'][0]['_id']
@@ -76,16 +76,14 @@ def index_doc_from_elem(elem, filter_words_df, filename):
 def load_pubmed_updates_v2():
 	print('called function')
 	es = u.get_es_client()
-	number_of_processes = mp.cpu_count()
-	pool = Pool(processes=1)
+
+	pool = Pool(processes=60)
 
 	cursor = pg.return_postgres_cursor()
 	
 	filter_words_query = "select words from annotation.filter_words"
 	filter_words_df = pg.return_df_from_query(cursor, filter_words_query, None, ["words"])
 	cursor.close()
-	print('closed cursor')
-	
 
 	index_exists = es.indices.exists(index=INDEX_NAME)
 	if not index_exists:
@@ -94,44 +92,40 @@ def load_pubmed_updates_v2():
 
 	s3 = boto3.resource('s3')
 	bucket = s3.Bucket('pubmed-baseline-1')
-	# for object in bucket.objects.all():
-		# bucket.download_file(object.key, object.key)
+	for object in bucket.objects.all():
+		bucket.download_file(object.key, object.key)
 
-	bucket.download('medline17n0600.xml', 'medline17n0600.xml')
-	file_timer = u.Timer('file')
+	
+		file_timer = u.Timer('file')
 
-		# tree = ET.parse(object.key)
-	tree = ET.parse('medline17n0600.xml')
-	root = tree.getroot()
+		tree = ET.parse(object.key)		
+		root = tree.getroot()
 
-	file_abstract_counter = 0
-	for elem in root:
+		file_abstract_counter = 0
 
-		if elem.tag == 'PubmedArticle':
+		for elem in root:
+			if elem.tag == 'PubmedArticle':
+				pool.apply_async(index_doc_from_elem, (elem, filter_words_df, object.key))
+				file_abstract_counter += 1
 
-			pool.apply_async(index_doc_from_elem, (elem, filter_words_df, object.key))
-			file_abstract_counter += 1
+			elif elem.tag == 'DeleteCitation':
+				delete_pmid_arr = get_deleted_pmid(elem)
 
-		elif elem.tag == 'DeleteCitation':
+				for pmid in delete_pmid_arr:
+					get_article_query = {'_source': ['id', 'pmid'], 'query': {'constant_score': {'filter' : {'term' : {'pmid': pmid}}}}}
+					query_result = es.search(index=INDEX_NAME, body=get_article_query)
 
-			delete_pmid_arr = get_deleted_pmid(elem)
-
-			for pmid in delete_pmid_arr:
-				get_article_query = {'_source': ['id', 'pmid'], 'query': {'constant_score': {'filter' : {'term' : {'pmid': pmid}}}}}
-				query_result = es.search(index=INDEX_NAME, body=get_article_query)
-
-				if query_result['hits']['total'] == 0:
-					continue
-				elif query_result['hits']['total'] == 1:
-					article_id = query_result['hits']['hits'][0]['_id']
-					es.delete(index=INDEX_NAME, doc_type='abstract', id=article_id)
-				else:
-					print("delete: more than one document found")
-					print(pmid)
+					if query_result['hits']['total'] == 0:
+						continue
+					elif query_result['hits']['total'] == 1:
+						article_id = query_result['hits']['hits'][0]['_id']
+						es.delete(index=INDEX_NAME, doc_type='abstract', id=article_id)
+					else:
+						print("delete: more than one document found")
+						print(pmid)
+		os.remove(object.key)		
+		file_timer.stop()
 		
-	os.remove(object.key)	
-			
-	file_timer.stop()
 	pool.close()
 	pool.join()
 
