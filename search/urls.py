417eec07be50fb694860a77a20656309958f1d57
Nima Karamooz <nkaramooz@gmail.com>
2018-03-17 13:34:00 -0700

    update vancocalc

--- search/urls.py
+++ search/urls.py
@@ -21,6 +21,7 @@ urlpatterns = [
     url(r'^concept_search/$', views.concept_search_results,  name='concept_search_results'),
     url(r'^concept_override/post/$', views.post_concept_override, name='post_concept_override'),
     url(r'^concept_override/', views.concept_override, name='concept_override'),
+    url(r'^vancocalc/$', views.vancomycin, name='vancomycin'),
     url(r'^ajax/vcSubmit/$', views.vcSubmit, name='vcSubmit'),
     url(r'^ajax/vcTroughTarget/$', views.returnTroughTarget, name='returnTroughTarget'),
 ]
