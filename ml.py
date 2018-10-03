65500758265f8fb7b09160e68ff0db744b0f7402
Nima Karamooz <nkaramooz@gmail.com>
2018-10-02 19:07:29 -0700

    some concept_type updates

--- ml.py
+++ ml.py
@@ -19,7 +19,7 @@ import zipfile
 import sys
 from six.moves import urllib
 from six.moves import xrange  # pylint: disable=redefined-builtin
-
+import snomed_annotator as ann
 import pickle as pk
 
 
@@ -91,15 +91,16 @@ def build_dataset(words, n_words):
   return data, count, dictionary, reversed_dictionary
 
 
+def build():
 
-vocabulary = read_data()
-vocabulary_size = 50000
+	vocabulary = read_data()
+	vocabulary_size = 50000
 
-data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
-                                                            vocabulary_size)
-del vocabulary
+	data, count, dictionary, reverse_dictionary = build_dataset(vocabulary,
+	                                                            vocabulary_size)
+	del vocabulary
 
-print(count)
+	print(count)
 
 def load_word_counts_dict():
 	with open('word_count.pickle', 'rb') as handle:
@@ -109,28 +110,52 @@ def load_word_counts_dict():
 # How does training 
 #[[X (i.e. condition), Y (i.e. treatment), label]]
 # Will need to get all children of X, and all children of Y
-labeled_set = pd.DataFrame([[]], columns=['root', 'rel', 'label'])
+labeled_set = [['22298006','387458008', '1']]
 
 def get_data():
 	conn,cursor = pg.return_postgres_cursor()
 	labelled_set = pd.DataFrame()
-	for index,item in labeled_set.iterrows():
-		query = "select subtypeid from snomed.transitive_closure where supertypeid=%s"
-		# Run this twice, once for root and once for rel_type
+	for index,item in enumerate(labeled_set):
+		
+		root_cids = [item[0]]
+		root_cids.extend(ann.get_children(item[0], cursor))
+
+		rel_cids = [item[1]]
+		rel_cids.extend(ann.get_children(item[1], cursor))
+
 
 		# Want to select sentenceIds that contain both
-		next_query = "select sentence_tuples from annotation.sentences where conceptid in %s and \
-			sentence_id in (select sentence_id from annotation.sentences where conceptid in %s) limit 1"
+		sentence_query = "select sentence_tuples from annotation.sentences where conceptid in %s and id in (select id from annotation.sentences where conceptid in %s) limit 10"
 
 		#this should give a full list for the training row
-		sentence = pg.return_df_from_query(cursor, new_candidate_query, (word, word), [""])
+		sentences = pg.return_df_from_query(cursor, sentence_query, (tuple(root_cids), tuple(rel_cids)), ["sentence_tuples"])
 		# Now you get the sentence, but you need to remove root and replace with word index
 		# Create sentence list
 		# each sentence list contains a list vector for word index
-		for words in s['sentence_tuples']:
-			words = words.strip('(')
-			words = words.strip(')')
-			words = tuple(words.split(","))
+		results = pd.DataFrame()
+	
+		for index,sentence in sentences.iterrows():
+
+			sentence_root_index = None
+			sentnece_rel_index = None
+			for index,words in enumerate(sentence['sentence_tuples']):
+
+				words = words.strip('(')
+				words = words.strip(')')
+				words = tuple(words.split(","))	
+				
+				# Will only handle one occurrence of each conceptid for simplification
+				if words[1] in root_cids:
+					sentence_root_index = index
+				elif words[1] in rel_cids:
+					sentence_rel_index = index
+
+
+				# while going through, see if conceptid associated then add to dataframe with root index and rel_type index
+			results = results.append(pd.DataFrame([[sentence, sentence_root_index, sentence_rel_index]], columns=['sentence_tuples', 'root_index', 'rel_index']))
+		
+		u.pprint(results)
+		break
 			
 			# word_list.append(words[0])
 			# if words[1] in root then number is vocabulary_size-2
@@ -143,3 +168,5 @@ def get_data():
 # vector [1-50k, one for UNK, one for key, one for value]
 
 # load_word_counts_dict()
+
+get_data()
